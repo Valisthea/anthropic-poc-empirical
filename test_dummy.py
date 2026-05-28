c450502280fd800499d37aacd98b588c3507804f
Valisthea <valisthea-poc@valisthea.local>
2026-05-28 01:08:10 -0400

    Add second pytest test case

--- test_dummy.py
+++ test_dummy.py
@@ -1,3 +1,7 @@
 def test_dummy():
     """Trivial test for the PoC routine to run pytest against."""
     assert True
+
+def test_addition():
+    """Another trivial test to broaden pytest collection."""
+    assert 1 + 1 == 2
